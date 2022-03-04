6cfc87fab9af357e5084f0e51651e5de2aabcce8
root <wyatt.lemaster@gmail.com>
2022-03-04 06:57:35 +0000

    samplecode

--- project3/AUbatch.c
+++ project3/AUbatch.c
@@ -2,11 +2,26 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+#include <unistd.h> 
+#include <pthread.h>
 
-// struct prototypes 
 
+/* COMMENT BLOCK 
+# Refrences:
+# aubatch_sample.c Written by Xiao Qin 
+#
+#
+#
+# How to compile: 
+# gcc AUbatch.c -o Aubatch -lpthread
+#
+#
+#
+#
+#
+*/
 
-
+/*########## Data structures Start ##########*/
 //d1
 struct job_info_queue
 {
@@ -57,6 +72,8 @@ struct scheduling_policy
     //enum polices {FCFS, SJF, PRIORITY};
 };
 
+/*########## Data structures END ##########*/
+
 
 
 /*########## start of functions ##########*/
@@ -64,10 +81,10 @@ struct scheduling_policy
 void scheduing_module(struct scheduling_policy policy, struct workload_info workload, struct job_info_queue job_info);
 
 //2
-void dispatching_module();
+void *dispatching_module(void *ptr); // job execution
 
 //3
-void commandline_parser();
+void *commandline_parser(void *ptr); // scheduling and submission
 
 //4 
 void display_job_queue();
@@ -83,18 +100,58 @@ void automated_performance_evaluation();
 /*########## End of functions ##########*/
 
 
-/*########## Data structures ##########*/
 
+
+// practice function can be removed
+void *myThreadFun(void *vargp)
+{
+    sleep(1);
+    printf("Printing GeeksQuiz from Thread \n");
+    return NULL;
+}
+
+pthread_mutex_t queue_lock;
+pthread_cond_t  buf_not_full;
+pthread_cond_t buf_not_empty;
+
+u_int buf_head;  // u_int used to prevent negative numbers
+u_int buf_tail;
+u_int count;
+
+char *cmd_buffer[CMD_BUF_SIZE];
 
 
 
 int main( int argc, char *argv[])
 {
-struct job_info_queue job_queue;
-job_queue.priority = 2; 
-printf("%d",job_queue.priority);
+/* FROM aubtach_sample.c Written by Xiao Qin*/ 
+    pthread_t command_thread, executor_thread; /* Two concurrent threads */
+    char *message1 = "Command Thread";
+    char *message2 = "Executor Thread";
+    int  iret1, iret2;
+    /* Initilize count, two buffer pionters */
+    count = 0; 
+    buf_head = 0;  
+    buf_tail = 0; 
+    /* Create two independent threads:command and executors */
+    iret1 = pthread_create(&command_thread, NULL, commandline, (void*) message1);
+    iret2 = pthread_create(&executor_thread, NULL, executor, (void*) message2);
+    /* Initialize the lock the two condition variables */
+    pthread_mutex_init(&cmd_queue_lock, NULL);
+    pthread_cond_init(&cmd_buf_not_full, NULL);
+    pthread_cond_init(&cmd_buf_not_empty, NULL);
+     
+    /* Wait till threads are complete before main continues. Unless we  */
+    /* wait we run the risk of executing an exit which will terminate   */
+    /* the process and all threads before the threads have completed.   */
+    pthread_join(command_thread, NULL);
+    pthread_join(executor_thread, NULL); 
+    printf("command_thread returns: %d\n",iret1);
+    printf("executor_thread returns: %d\n",iret1);
+/*End of aubatch_sample*/
 
-printf("\ntesting git\n\n");
+
+
 return 0;
 }
 
@@ -124,10 +181,113 @@ void scheduing_module(struct scheduling_policy policy, struct workload_info work
 }
 
 //2
-void dispatching_module(){};
+void dispatching_module(){}; 
+
+void *executor(void *ptr) // exampel of a dispatching module
+{
+    char *message;
+    u_int i;
+    message = (char *) ptr;
+    printf("%s \n", message);
+    for (i = 0; i < NUM_OF_CMD; i++) {
+        /* lock and unlock for the shared process queue */
+        pthread_mutex_lock(&cmd_queue_lock);
+        printf("In executor: count = %d\n", count);
+        while (count == 0) {
+            pthread_cond_wait(&cmd_buf_not_empty, &cmd_queue_lock);
+        }
+        /* Run the command scheduled in the queue */
+        count--;
+        printf("In executor: cmd_buffer[%d] = %s\n", buf_tail, 
+cmd_buffer[buf_tail]); 
+        
+        /* 
+         * Note: system() function is a simple example.
+         * You should use execv() rather than system() here.
+         */
+        system(cmd_buffer[buf_tail]); 
+        /* Free the dynamically allocated memory for the buffer */
+        free(cmd_buffer[buf_tail]);
+#ifdef LOW_SERVICE_RATE
+        sleep(2); /* Simulate service time of 2 seconds */
+#endif
+     
+        /* Move buf_tail forward, this is a circular queue */ 
+        buf_tail++;
+        if (buf_tail == CMD_BUF_SIZE)
+            buf_tail = 0;
+        pthread_cond_signal(&cmd_buf_not_full);
+        /* Unlok the shared command queue */
+        pthread_mutex_unlock(&cmd_queue_lock);
+    } /* end for */
+}
+
+
+
+
+
 
 //3
 void commandline_parser(){};
+
+
+void *commandline(void *ptr) // example of commandline_parser
+{
+    char *message;
+    char *temp_cmd;
+    u_int i;
+    char num_str[8];
+    size_t command_size;
+     
+    message = (char *) ptr;
+    printf("%s \n", message);
+    /* Enter multiple commands in the queue to be scheduled */
+    for (i = 0; i < NUM_OF_CMD; i++) {    
+        /* lock the shared command queue */
+        pthread_mutex_lock(&cmd_queue_lock);
+ 
+        printf("In commandline: count = %d\n", count);
+        while (count == CMD_BUF_SIZE) {
+            pthread_cond_wait(&cmd_buf_not_full, &cmd_queue_lock);
+        }
+        /* Dynamically create a buffer slot to hold a commandline */
+#ifdef STATIC_COMMAND
+        cmd_buffer[buf_head] = malloc(strlen("process -help -time ") + 1);
+        strcpy(cmd_buffer[buf_head], "./process -help -time "); 
+        sprintf(num_str, "%d", i);
+        strcat(cmd_buffer[buf_head], num_str);
+#else
+        pthread_mutex_unlock(&cmd_queue_lock);
+        printf("Please submit a batch processing job:\n");
+        printf(">"); 
+        temp_cmd = malloc(MAX_CMD_LEN*sizeof(char));
+        getline(&temp_cmd, &command_size, stdin);  
+        pthread_mutex_lock(&cmd_queue_lock);    
+        cmd_buffer[buf_head]= temp_cmd; 
+        
+#endif
+        printf("In commandline: cmd_buffer[%d] = %s\n", buf_head, 
+cmd_buffer[buf_head]);  
+    
+        count++;
+ 
+        /* Move buf_head forward, this is a circular queue */ 
+        buf_head++;
+        if (buf_head == CMD_BUF_SIZE)
+            buf_head = 0;
+        pthread_cond_signal(&cmd_buf_not_empty);  
+        /* Unlok the shared command queue */
+        pthread_mutex_unlock(&cmd_queue_lock);
+        /* Simulate a low arrival rate */
+#ifdef LOW_ARRIVAL_RATE
+        sleep(2); /* Simulate an arrival time of 2 seconds */
+#endif
+    } /* end for */
+}
+
+
+
+
 
 //4 
 void display_job_queue()
--- project3/sample_code/pthread.c
+++ project3/sample_code/pthread.c
@@ -4,8 +4,24 @@
 #include <sys/types.h>
 
 
+/*
+Thread practice for assignment 3
+simple refrence: 
+    https://www.thegeekstuff.com/2012/04/create-threads-in-linux/
 
-void* function_one()
+Really good youtube channel:
+    https://www.youtube.com/watch?v=ln3el6PR__Q&list=PLfqABt5AS4FmuQf70psXrsMLEDQXNkLq2&index=6
+
+
+
+Things to do: 
+    create an array of strings where the standard producer consumer problem is solved using threads and CV 
+    This will stand as the basis behind assignment 3 
+*/
+
+
+
+void* proccess_exec()
 {
     
 char *my_args[5];
@@ -18,25 +34,54 @@ my_args[3] = NULL;
       puts("Uh oh! If this prints, execv() must have failed");
 };
 
-
-void* function_two()
+void* function_one(void *arg)
 {
-printf("inside thread two\n");
-};
+printf("inside thread one, adding values strings containing fruit\n");
+//char *arr = (int * )queue;
+char * queue = malloc(100); // this must be malloc because when the function ends it frees the memory and deletes our data since we are just passing a pointer
+*queue = "Apple";
+return (void *) queue;
+}
+/*
+void* function_two(void* arg)
+{
+printf("inside thread two adding strings containing cars \n");
+//char *arr = (char * )queue;
 
+ *(char * )arg[4] = "F150";
+ *(char * )arg[5] = "maxima";
+ *(char * )arg[6] = "Dart";
+ *(char * )arg[7] = "Demon";
+
+
+}
+*/
 
 
 int main(int argc, char *argv[])
 {
 
+char * queue[100];
+
 pthread_t tid_one, tid_two;
 
-pthread_create(&tid_one, NULL, &function_one, NULL);
+pthread_create(&tid_one, NULL, &function_one, (void*) queue);
 
-
-pthread_create(&tid_two, NULL, &function_two, NULL);
-
+if(pthread_join(tid_one, (void**) &queue) !=0)
+{
+    return 2;
+}
+free(queue);
+//pthread_create(&tid_two, NULL, &function_two, (void*) queue);
 
 sleep(5);
+int i = 0; 
+for( i = 0; i < 1; i ++ ) 
+{
+printf("%s\n", queue[i]);
+
+}
+
+
 return 0;
 }
